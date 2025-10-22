cb27b13add0b0d12d28078c6d515cc512a45c03b
LEONARDO REIS DE CAMPOS <leonardo.campos@cs.up.edu.br>
2025-10-22 20:55:07 -0300

    GET POST

--- api/Program.cs
+++ api/Program.cs
@@ -8,6 +8,19 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/", async (AppDbContext db) =>
+    await db.Medicamentos.ToListAsync() );
+
+app.MapGet("/Medicamentos/{id}", async (int id, AppDbContext db) => {
+    var Medicamentos = await db.Medicamentos.FindAsync(id);
+    return Medicamentos is not null ? Results.Ok(Medicamentos) : Results.NotFound("Medicamento não encontrado");}
+    );
+
+app.MapPost("/Medicamentos", async (AppDbContext db, Medicamentos novoMedicamento) =>
+    {db.Medicamentos.Add(novoMedicamento);
+    await db.SaveChangesAsync();
+    return Results.Created($"/Medicamentos/{novoMedicamento.id}", novoMedicamento);
+    });
 app.Run();
