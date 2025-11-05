af933d15b3d9ac041b477352edba549f86a727d1
unknown <lanhacase@gmail.com`>
2025-11-05 06:28:43 -0800

    post deletado do main

--- web/js/main.js
+++ web/js/main.js
@@ -29,33 +29,3 @@ if (searchInput) {
     }
   });
 }
-
-// adicionar
-const adicionarButton = document.getElementById("adicionar");
-if (adicionarButton) { 
-  adicionarButton.onclick = async () => {
-        const nome = document.getElementById("nome").value;
-        const fabricante = document.getElementById("fabricante").value;
-        const categoria = document.getElementById("categoria").value;
-
-        const novoMedicamento = {
-          nome,
-          fabricante,
-          categoria,
-          descricao: "Exemplo simples",
-          principioAtivo: "Substância exemplo",
-          indicacoes: "Indicações exemplo",
-          contraIndicacoes: "Contraindicações exemplo",
-          imagem: "https://exemplo.com/imagem.png"
-        };
-
-        const res = await fetch(`${API_URL}/Medicamentos`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(novoMedicamento)
-        });
-
-        const data = await res.json();
-        console.log("Adicionado:", data);
-      };
-}
